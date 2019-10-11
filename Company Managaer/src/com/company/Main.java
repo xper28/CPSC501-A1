5f9b62b244839b72857f4a91ed29953302ab69df
xper28 <ahmad.hassan1@ucalgary.ca>
2019-10-11 14:36:23 -0600

    Broke up long main method into smaller methods through extraction

--- Company Managaer/src/com/company/Main.java
+++ Company Managaer/src/com/company/Main.java
@@ -11,56 +11,74 @@ public class Main {
 		ArrayList<Department> dep = new ArrayList<>();
 		ArrayList<Salaried> salaried = new ArrayList<>();
 		ArrayList<Hourly> hourly = new ArrayList<>();
-		System.out.println("Welcome to Company Manager\n press any key then enter to continue" );
-
+		System.out.println("Welcome to Company Manager\n choose option to continue" );
 		while(true){
-			System.out.println("1)Create Salaried employee\n" +
-					"2)Create hourly employee\n" +
-					"3)Create Department\n" +
-					"4)View Departments\n");
-			System.out.println("0)Exit");
-			//Replace temp with query
+			mainMenu();
 			int temp = kbd.nextInt();
-			//if(kbd.next())
 			if(temp == 1){
-				System.out.println("Enter first name");
-				String fname = kbd.next();
-				System.out.println("Enter last name");
-				String lname = kbd.next();
-				System.out.println("Enter title");
-				String title = kbd.next();
-				System.out.println("Enter monthly salary");
-				int monthly = kbd.nextInt();
-				System.out.println("Enter id number");
-				int id = kbd.nextInt();
-				Salaried emp = new Salaried(fname,lname,title,id, monthly);
-				salaried.add(emp);
+				salariedPrompt(kbd, salaried);
 			}else if(temp == 2){
-				System.out.println("Enter first name");
-				String fname = kbd.next();
-				System.out.println("Enter last name");
-				String lname = kbd.next();
-				System.out.println("Enter title");
-				String title = kbd.next();
-				System.out.println("Enter hourly rate");
-				double rate = kbd.nextDouble();
-				System.out.println("Enter id number");
-				int id = kbd.nextInt();
-				Hourly emp = new Hourly(fname,lname,title,id, rate);
-				hourly.add(emp);
+				hourlyPrompt(kbd, hourly);
 			}else if(temp == 3){
-				System.out.println("Enter department name");
-				String dName = kbd.next();
-				Department dept = new Department(dName);
-				dep.add(dept);
-				System.out.println("Created: " + dept.name + " successfully!");
+				deptPrompt(kbd, dep);
 			}else if(temp == 4){
-				for(int i=0; i<dep.size(); i++){
-					System.out.println(i+ ") " + dep.get(i).name);
-				}
+				showDepts(dep);
 			}else if(temp == 0){
 				break;
 			}
 		}
 	}
+	public static void mainMenu(){
+		System.out.println("1)Create Salaried employee\n" +
+				"2)Create hourly employee\n" +
+				"3)Create Department\n" +
+				"4)View Departments");
+		System.out.println("0)Exit");
+	}
+
+	public static void salariedPrompt(Scanner kbd, ArrayList<Salaried> salaried){
+		System.out.println("Enter first name");
+		String fname = kbd.next();
+		System.out.println("Enter last name");
+		String lname = kbd.next();
+		System.out.println("Enter title");
+		String title = kbd.next();
+		System.out.println("Enter monthly salary");
+		int monthly = kbd.nextInt();
+		System.out.println("Enter id number");
+		int id = kbd.nextInt();
+		Salaried emp = new Salaried(fname,lname,title,id, monthly);
+		salaried.add(emp);
+	}
+
+	public static void hourlyPrompt(Scanner kbd, ArrayList<Hourly> hourly){
+		System.out.println("Enter first name");
+		String fname = kbd.next();
+		System.out.println("Enter last name");
+		String lname = kbd.next();
+		System.out.println("Enter title");
+		String title = kbd.next();
+		System.out.println("Enter hourly rate");
+		double rate = kbd.nextDouble();
+		System.out.println("Enter id number");
+		int id = kbd.nextInt();
+		Hourly emp = new Hourly(fname,lname,title,id, rate);
+		hourly.add(emp);
+	}
+
+	public static void deptPrompt(Scanner kbd, ArrayList<Department> dep){
+		System.out.println("Enter department name");
+		String dName = kbd.next();
+		Department dept = new Department(dName);
+		dep.add(dept);
+		System.out.println("Created: " + dept.name + " successfully!");
+	}
+
+	public static void showDepts(ArrayList<Department> dep){
+		for(int i=0; i<dep.size(); i++){
+			System.out.println(i+ ") " + dep.get(i).name);
+		}
+		System.out.println();
+	}
+
 }
